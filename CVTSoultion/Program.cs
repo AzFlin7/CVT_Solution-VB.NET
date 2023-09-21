6cb6ab1132c01478a5ab27ac20d2268544295b1b
yuzh37 <yusoffzhang@gmail.com>
2023-09-21 21:09:16 +0800

    started working localization

--- CVTSoultion/Program.cs
+++ CVTSoultion/Program.cs
@@ -1,18 +1,26 @@
 using CVTSoultion.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddLocalization();
+//builder.Services.AddSingleton<LocalizationMiddleware>();
+//builder.Services.AddDistributedMemoryCache();
+//builder.Services.AddSingleton<IStringLocalizerFactory, JsonStringLocalizerFactory>();
 
 var app = builder.Build();
 
@@ -27,6 +35,13 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseRequestLocalization(options => new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(new CultureInfo("en-US"))
+});
+//app.UseMiddleware<LocalizationMiddleware>();
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
